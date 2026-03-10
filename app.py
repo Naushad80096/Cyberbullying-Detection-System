0b5e6983092be1273919be52aa2af1d5ae12afc7
Naushad80096 <naushadmgs7@gmail.com>
2026-03-10 14:39:07 +0530

    Fixed prediction pipeline using TF-IDF vectorizer

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 import streamlit as st
 import joblib
-import pandas as pd
+import re
 
 st.title("Cyberbullying Detection System")
 
 st.write("Enter a social media comment to detect cyberbullying.")
 
 model = joblib.load("models/cyberbullying_model.pkl")
+vectorizer = joblib.load("models/vectorizer.pkl")
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z ]", "", text)
+    return text
+
 
 text = st.text_area("Enter comment")
 
 if st.button("Predict"):
 
-    prediction = model.predict([text])
+    if text.strip() == "":
+        st.warning("Please enter a comment")
 
-    if prediction[0] == 1:
-        st.error("Cyberbullying Detected")
     else:
-        st.success("Normal Comment")
+
+        text = clean_text(text)
+
+        text_vector = vectorizer.transform([text])
+
+        prediction = model.predict(text_vector)
+
+        if prediction[0] == 1:
+            st.error("Cyberbullying Detected")
+        else:
+            st.success("Normal Comment")
